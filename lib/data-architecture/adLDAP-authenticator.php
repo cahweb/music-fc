4beaa387116c3a6737360537c8b3ccdd49bc7cac
Michael Leavitt <michael.leavitt@ucf.edu>
2020-02-11 14:14:36 -0500

    Fixing adLDAP

--- lib/data-architecture/adLDAP-authenticator.php
+++ lib/data-architecture/adLDAP-authenticator.php
@@ -6,7 +6,8 @@
  * @version 1.0.0
  */
 
-require_once MUSIC_FC__BASE_DIR . '/lib/adLDAP/lib/adLDAP/adLDAP.php';
+//require_once MUSIC_FC__BASE_DIR . '/lib/adLDAP/lib/adLDAP/adLDAP.php';
+require_once 'NET/adLDAP.php';
 
 if( !interface_exists( 'AdLDAPAuthenticator' ) ) {
     interface AdLDAPAuthenticator
